8567be1bb91533baec985f4640f1ded79282e396
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-03-25 16:30:42 +0545

    fixed QR page

--- src/app/qrcode-page/page.tsx
+++ src/app/qrcode-page/page.tsx
@@ -3,29 +3,25 @@ import Image from "next/image";
 const QRPage = () => {
   return (
     <>
-      <div className="flex justify-center shadow-lg mx-auto mt-20">
-        <section className="text-gray-600 body-font my-20">
-          <div className="container mx-auto flex px-auto py-24 items-center justify-center flex-col shadow-blue-500 shadow-2xl">
-            <Image
-              className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
-              alt="qr code"
-              src="/qr.webp"
-              width={300}
-              height={300}
-            />
-            <div className="text-center lg:w-2/3 w-full">
-              <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-                Event Name
-              </h1>
-              <div></div>
-              <div className="flex justify-center">
-                <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                  QR CODE
-                </button>
-              </div>
-            </div>
+      <div className="flex justify-center  mx-auto mt-20 py-4">
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-lg">
+          {/* Event Image  */}
+          <Image
+            className="rounded-2xl p-3"
+            alt="qr code"
+            src="/qr.webp"
+            width={400}
+            height={400}
+          />
+
+          <div className="p-5">
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                Events name
+              </h5>
+            </a>
           </div>
-        </section>
+        </div>
       </div>
     </>
   );
