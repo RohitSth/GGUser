d6d95d943bfc0b2f95985eebed6985b8fe5f5437
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-04-01 20:14:17 +0545

    made the card to flip when clicked

--- src/app/components/QRCodeComponent.tsx
+++ src/app/components/QRCodeComponent.tsx
@@ -1,25 +1,21 @@
 "use client";
 
+import { usePathname } from "next/navigation";
+
 import Image from "next/image";
 import { useState } from "react";
 import QRCode from "qrcode";
 
 const QRCodeComponent = () => {
-  // const [username, setUsername] = useState("");
   const [src, setSrc] = useState("");
-
-  const generateQR = () => {
-    // QRCode.toDataURL(`https://github.com/${username}`).then(setSrc);
-    QRCode.toDataURL("https://github.com/RohitSth").then(setSrc);
-  };
+  const pathname = usePathname();
+  QRCode.toDataURL(pathname).then(setSrc);
 
   return (
     <>
       <Image
         className="object-cover rounded-xl mx-auto"
         alt="qr code"
-        onClick={generateQR}
-        // src="/qr.webp"
         src={src}
         width={200}
         height={200}
--- src/app/ticket/page.tsx
+++ src/app/ticket/page.tsx
@@ -1,14 +1,27 @@
+"use client";
+
 import Image from "next/image";
 import EventDetails from "../components/EventDetails";
 import QRCodeComponent from "../components/QRCodeComponent";
+import { useState } from "react";
 
 const TicketPage = () => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
   return (
     <>
-      <div className="flex justify-center  mx-auto mt-8 py-4">
+      <div className="flex justify-center mx-auto mt-8 py-4">
         {/* Event Image  */}
         <div className="group h-72 w-96 p-3 [prespective:1000px]">
-          <div className="relative h-full w-full rounded-xl shadow-md transition-all duration-500  [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
+          {/* Flip the card when clicked */}
+          <div
+            className={`relative h-full w-full rounded-xl shadow-md transition-all duration-500  [transform-style:preserve-3d] 
+            ${isFlipped ? "[transform:rotateY(180deg)]" : ""}`}
+            onClick={handleFlip}
+          >
             <div className="absolute inset-0">
               <Image
                 className="object-cover shadow-black/40 rounded-lg shadow-xl"
@@ -28,13 +41,6 @@ const TicketPage = () => {
               </div>
             </div>
             <div className="absolute inset-0 flex items-center h-full w-full rounded-lg bg-black/75 px-12 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]">
-              {/* <Image
-                className="object-cover rounded-xl mx-auto"
-                alt="event's picture"
-                src="/qr.webp"
-                width={200}
-                height={200}
-              /> */}
               <QRCodeComponent />
             </div>
           </div>
